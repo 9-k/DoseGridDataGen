d1c655693ba729e5392e08da4ce6b9514ae3c25b
9K <schrian25@gmail.com>
2024-10-22 17:02:23 -0500

    actually you don't need ilmerge for .exes!

--- DoseGridDataGen.cs
+++ DoseGridDataGen.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Collections.Generic;
@@ -16,26 +17,65 @@ using VMS.TPS.Common.Model.Types;
 
 namespace DoseGridDataGen
 {
-  class Program
-  {
-    [STAThread]
-    static void Main(string[] args)
+    class Program
     {
-      try
-      {
-        using (Application app = Application.CreateApplication())
+        [STAThread]
+        static void Main(string[] args)
         {
-          Execute(app);
+            try
+            {
+                using (Application app = Application.CreateApplication())
+                {
+                    Execute(app);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+            }
         }
-      }
-      catch (Exception e)
-      {
-        Console.Error.WriteLine(e.ToString());
-      }
+        static void Execute(Application app)
+        {
+            string settings = "settings.txt";
+            string assyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string settingsPath = Path.Combine(assyPath, settings);
+
+            Dictionary<string, string> settingsDict = new Dictionary<string, string>();
+
+            foreach (var line in File.ReadLines(settingsPath))
+            {
+                var parts = line.Split(new[] { ':' }, 2);
+                if (parts.Length == 2)
+                {
+                    string key = parts[0].Trim();
+                    string value = parts[1].Trim();
+                    settingsDict[key] = value;
+                }
+                else
+                {
+                    throw new ApplicationException("Malformed settings.txt file. Ensure each line of of the form *:*?");
+                }
+            }
+
+            string mrnPath = Path.Combine(assyPath, settingsDict["mrnFile"]);
+            string tg263Path = Path.Combine(assyPath, settingsDict["tg263File"]);
+            string dummyPtId = settingsDict["dummyPtId"];
+
+            IEnumerable<string> mrnList = File.ReadLines(mrnPath)
+                .Select(line => line.Trim())
+                .Where(line => !string.IsNullOrEmpty(line));
+
+
+            int courseCounter = 0;
+            int patientCounter = 0;
+            foreach (string mrn in mrnList)
+            {
+                Patient patient = app.OpenPatientById(mrn);
+                
+
+
+                app.ClosePatient();
+            }
+        }
     }
-    static void Execute(Application app)
-    {
-      // TODO: Add your code here.
-    }
-  }
 }
