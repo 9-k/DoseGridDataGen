c80dd9ae604e4e02d16f473d2f1e3d9ef0014817
9K <schrian25@gmail.com>
2024-10-23 15:41:00 -0500

    added tracked params, skeleton for patient iterator, for utilities, added structureparser

--- DoseGridDataGen.cs
+++ DoseGridDataGen.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
+using ESAPIUtilities;
 
 // TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
 [assembly: AssemblyVersion("1.0.0.1")]
@@ -36,46 +37,76 @@ namespace DoseGridDataGen
         }
         static void Execute(Application app)
         {
-            string settings = "settings.txt";
-            string assyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string settingsPath = Path.Combine(assyPath, settings);
+            // you absolutely need to ensure this list matches with the database crawl list
+            List<string> ICDCODELIST = new List<string>()
+                {
+                    "C44",
+                    "C69",
+                    "C70",
+                    "C71",
+                    "C72",
+                    "C75.1",
+                    "C76",
+                    "C79.3",
+                    "C96",
+                };
 
-            Dictionary<string, string> settingsDict = new Dictionary<string, string>();
-
-            foreach (var line in File.ReadLines(settingsPath))
+            List<string> TRACKEDOARS = new List<string>()
             {
-                var parts = line.Split(new[] { ':' }, 2);
-                if (parts.Length == 2)
-                {
-                    string key = parts[0].Trim();
-                    string value = parts[1].Trim();
-                    settingsDict[key] = value;
-                }
-                else
-                {
-                    throw new ApplicationException("Malformed settings.txt file. Ensure each line of of the form *:*?");
-                }
-            }
 
-            string mrnPath = Path.Combine(assyPath, settingsDict["mrnFile"]);
-            string tg263Path = Path.Combine(assyPath, settingsDict["tg263File"]);
+            };
+
+            List<string> TRACKEDPARAMS = new List<string>()
+            {
+                "Target FMAID",
+                "OAR FMAID",
+                "Target Volume",
+                "OAR Volume",
+                "Minimum Separation",
+                "Centroid Separation",
+                "Prescription Dose To Target",
+                "True Dose To Target",
+                "Grid Size", 
+                "MRN",
+                "Course Name",
+                "Plan Name"
+            };
+
+            string settingsPath = "settings.txt";
+            string assyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            Dictionary <string,string> settingsDict = ESAPIUtility.SettingsDict(settingsPath);
+
+            string mrnPath = Path.GetFullPath(settingsDict["mrnPath"]);
+            string TG263Path = Path.GetFullPath(settingsDict["TG263Path"]);
             string dummyPtId = settingsDict["dummyPtId"];
 
             IEnumerable<string> mrnList = File.ReadLines(mrnPath)
                 .Select(line => line.Trim())
                 .Where(line => !string.IsNullOrEmpty(line));
 
-
             int courseCounter = 0;
             int patientCounter = 0;
             foreach (string mrn in mrnList)
             {
                 Patient patient = app.OpenPatientById(mrn);
-                
+                List<Course> coursesMatchingDxList =
+                    patient.Courses
+                    .Where(course => course.Diagnoses
+                    .Any(dx => ICDCODELIST.Any(code => dx.Code.Contains(code)))).ToList();
 
+                foreach (Course course in coursesMatchingDxList)
+                {
+                    foreach (ExternalPlanSetup plan in course.ExternalPlanSetups)
+                    {
+                        var target = plan.TargetVolumeID;
 
+                    }
+                    courseCounter++;
+                }
                 app.ClosePatient();
             }
         }
+
     }
 }
---
+++ StructureParser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Reflection;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using ESAPIUtilities;
+using FuzzySharp;
+using System.Windows;
+using System.Text.RegularExpressions;
+
+namespace DoseGridDataGen
+{
+    public struct TG263ParseResult
+    {
+        public bool IsEval { get; set; }
+        public bool IsOpti { get; set; }
+        public bool IsDerived { get; set; }
+        public bool IsPlanning { get; set; }
+        public bool IsPRV { get; set; }
+        public List<ValueTuple<DataRow, int>> matches { get; set; }
+    }
+
+    public class StructureParserMethods
+    {
+        public static TG263ParseResult StructureParser(string structName, DataTable TG263Table, int topN=1, float cutoff=0)
+        {
+            TG263ParseResult result = new TG263ParseResult();
+            structName.Trim();
+            structName.TrimStart('z', 'Z', '_');
+            string sup = structName.ToUpper();
+            if (sup.Contains("EVAL"))
+            {
+                result.IsEval = true;
+            }
+            if (sup.Contains("-") || sup.Contains("+"))
+            {
+                result.IsDerived = true;
+            }
+            // if it starts with 'p' and it matches better if I remove p,
+            if (structName.StartsWith("p") && 
+                StructureParser(structName.TrimStart('p'), TG263Table).matches.First().Item2 > StructureParser(structName, TG263Table).matches.First().Item2)
+            {
+                result.IsPlanning = true;
+            }
+
+            int numOpts = Regex.Matches(sup, "OPT").Count;
+            bool contOptic = sup.Contains("OPTIC");
+
+            // if there's exactly one instance of OPT and the string contains OPTIC
+            if (!(numOpts == 1 && contOptic))
+            {
+                result.IsOpti = true;
+            }
+            if (sup.Contains("PRV"))
+            {
+                result.IsPRV = true;
+            }
+
+            if (topN >= 1)
+            {
+                var dists = new List<ValueTuple<string, int>>();
+                List<ValueTuple<DataRow, int>> matches = new List<ValueTuple<DataRow, int>>();
+
+                foreach (DataRow row in TG263Table.Rows)
+                {
+                    // gotta match to upper so that casing doesn't make an issue
+                    string TG263name = row["TG263-Primary Name"].ToString().ToUpper();
+                    int dist = Fuzz.WeightedRatio(sup, TG263name);
+                    dists.Add((TG263name, dist));
+                }
+
+                dists.OrderByDescending(x => x.Item2).Take(topN);
+
+                foreach (ValueTuple<string, int> dist in dists)
+                {
+                    DataRow match = TG263Table.Select($"Name = '{dist.Item1}'").First();
+                    matches.Add((match, dist.Item2));
+                }
+                result.matches = matches;
+            }
+
+            // apply cutoff 
+            result.matches.RemoveAll(element => element.Item2 < cutoff);
+            return result;
+        }
+    }
+}
