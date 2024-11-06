984324bc4cccb35817d55bb35a3de997769be7ff
9K <schrian25@gmail.com>
2024-11-06 14:55:10 -0600

    asdf

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.18")]
-[assembly: AssemblyFileVersion("1.0.0.18")]
+[assembly: AssemblyVersion("1.0.0.21")]
+[assembly: AssemblyFileVersion("1.0.0.21")]
 [assembly: AssemblyInformationalVersion("1.0")]
